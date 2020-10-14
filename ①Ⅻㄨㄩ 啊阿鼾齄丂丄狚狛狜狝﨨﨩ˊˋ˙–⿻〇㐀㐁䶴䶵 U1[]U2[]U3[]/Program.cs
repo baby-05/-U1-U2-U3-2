91b18a119f461f6e595c9bfdbac0286d6901d0d9
Aimee Cao <v-liucao@microsoft.com>
2020-10-14 06:17:13 +0000

    Add project files.

---
+++ ①Ⅻㄨㄩ 啊阿鼾齄丂丄狚狛狜狝﨨﨩ˊˋ˙–⿻〇㐀㐁䶴䶵 U1[]U2[]U3[]/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace __ㄨㄩ_啊阿鼾齄丂丄狚狛狜狝﨨﨩ˊˋ____㐀㐁䶴䶵_U1_______U2______U3_______
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
